5dd3a8f1a749768e9c3bc8f71842b9b805f2316a
ksjhiu <94771016+ksjhiu@users.noreply.github.com>
2022-03-31 10:42:15 +0530

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,18 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+
+
+list1 = [12,-7,5,64,-14]
+
+# iterating each number in list
+for num in list1:
+	
+	# checking condition
+	if num >= 0:
+	print(num, end = " ")
